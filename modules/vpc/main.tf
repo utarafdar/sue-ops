9bfe4dcf979251aca2128e8b901522e77b4827dd
utarafdar <umair.tarafdar@gmail.com>
2025-03-29 16:30:00 +0530

    Add NAT Gateway, route tables, and subnet associations for VPC

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -58,14 +58,40 @@ resource "aws_route_table_association" "public" {
   route_table_id = aws_route_table.public.id
 }
 
-# Private Route Table
-resource "aws_route_table" "private" {
-  vpc_id = aws_vpc.main.id
+# Create Elastic IPs for NAT Gateways
+resource "aws_eip" "nat" {
+  count = length(var.azs)
 }
 
-# Associate Private Subnets with Private Route Table
+# Create NAT Gateways in Public Subnets
+resource "aws_nat_gateway" "nat" {
+  count         = length(var.azs)
+  allocation_id = aws_eip.nat[count.index].id
+  subnet_id     = aws_subnet.public[count.index].id
+
+  tags = {
+    Name = "${var.vpc_name}-nat-${count.index + 1}"
+  }
+}
+
+# Create Separate Route Tables for Each Private Subnet
+resource "aws_route_table" "private" {
+  count  = length(var.azs)
+  vpc_id = aws_vpc.main.id
+
+  route {
+    cidr_block     = "0.0.0.0/0"
+    nat_gateway_id = aws_nat_gateway.nat[count.index].id
+  }
+
+  tags = {
+    Name = "${var.vpc_name}-private-rt-${count.index + 1}"
+  }
+}
+
+# Associate Private Subnets with Their Route Tables
 resource "aws_route_table_association" "private" {
   count          = length(var.private_subnet_cidrs)
   subnet_id      = aws_subnet.private[count.index].id
-  route_table_id = aws_route_table.private.id
+  route_table_id = aws_route_table.private[count.index].id
 }
